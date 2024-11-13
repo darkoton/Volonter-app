03f97b987f9d4f92282f82676a3263614a184148
Your Name <you@example.com>
2024-11-14 01:06:29 +0200

    add autocomplete to form auth

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -1,19 +1,17 @@
 <script setup>
-import { Form, Field, ErrorMessage } from 'vee-validate';
-import { useRouter } from 'vue-router'
-import * as yup from 'yup';
-import { useAlertStore } from '@stores/alert.js'
-import { login } from '@plugins/firebase/auth.js'
-import { useUserStore } from '@stores/user.js'
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { useRouter } from "vue-router";
+import * as yup from "yup";
+import { useAlertStore } from "@stores/alert.js";
+import { login } from "@plugins/firebase/auth.js";
+import { useUserStore } from "@stores/user.js";
 
-const alertStore = useAlertStore()
-const router = useRouter()
-const userStore = useUserStore()
-
-
+const alertStore = useAlertStore();
+const router = useRouter();
+const userStore = useUserStore();
 
 if (userStore.authorized) {
-  router.push("/profile")
+  router.push("/profile");
 }
 
 const schema = yup.object({
@@ -26,25 +24,24 @@ const form = [
     title: "Ваший email",
     name: "email",
     placeholder: "name@company.com",
-    type: "email"
+    type: "email",
   },
   {
     title: "Пароль",
     name: "password",
     placeholder: "••••••••",
-    type: "password"
+    type: "password",
   },
-
-]
+];
 
 async function submit(values, { resetForm }) {
   try {
-    await login(values.email, values.password)
-    alertStore.showAlert("Ви успішно увійшли до облікового запису!", "success")
-    resetForm()
-    router.push('/profile')
+    await login(values.email, values.password);
+    alertStore.showAlert("Ви успішно увійшли до облікового запису!", "success");
+    resetForm();
+    router.push("/profile");
   } catch (err) {
-    alertStore.showAlert(err.message, "error")
+    alertStore.showAlert(err.message, "error");
   }
 }
 </script>
@@ -52,20 +49,38 @@ async function submit(values, { resetForm }) {
 <template>
   <div class="login h-full flex items-center">
     <div class="container flex justify-center">
-      <div class="w-full bg-turquoise-900 rounded-lg shadow border-turquoise-700 sm:max-w-md xl:p-0">
+      <div
+        class="w-full my-[30px] bg-turquoise-900 rounded-lg shadow border-turquoise-700 sm:max-w-md xl:p-0"
+      >
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl"
+          >
             Увійдіть у свій обліковий запис
           </h1>
-          <Form class="space-y-2 md:space-y-4" @submit="submit" :validation-schema="schema"
-            v-slot="{ errors, isSubmitting }">
+          <Form
+            class="space-y-2 md:space-y-4"
+            @submit="submit"
+            :validation-schema="schema"
+            v-slot="{ errors, isSubmitting }"
+          >
             <div v-for="fieldData in form" :key="fieldData.name">
-              <label :for="fieldData.name" class="block mb-2 text-sm font-medium text-white">
+              <label
+                :for="fieldData.name"
+                class="block mb-2 text-sm font-medium text-white"
+              >
                 {{ fieldData.title }}
               </label>
-              <Field :type="fieldData.type" :name="fieldData.name" :id="fieldData.name"
+              <Field
+                autocomplete
+                :type="fieldData.type"
+                :name="fieldData.name"
+                :id="fieldData.name"
                 class="bg-turquoise-700 border-turquoise-600 text-white text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-full p-2.5 placeholder-turquoise-400"
-                :class="errors[fieldData.name] && 'border-red-400'" :placeholder="fieldData.placeholder" required />
+                :class="errors[fieldData.name] && 'border-red-400'"
+                :placeholder="fieldData.placeholder"
+                required
+              />
               <ErrorMessage :name="fieldData.name" class="text-red-400" />
             </div>
             <!-- <div class="flex items-center justify-between">
@@ -85,11 +100,19 @@ async function submit(values, { resetForm }) {
             </button>
             <p class="text-sm font-light text-turquoise-400">
               Ще не маєте облікового запису?
-              <router-link :to="{ name: 'user-register' }" class="font-medium link">Зареєструватися</router-link>
+              <router-link
+                :to="{ name: 'user-register' }"
+                class="font-medium link"
+                >Зареєструватися</router-link
+              >
             </p>
             <p class="text-sm !mt-[5px] font-light text-turquoise-400">
               Хочете стати волонтером?
-              <router-link :to="{ name: 'volunteer-register' }" class="font-medium link">Стати волонтером</router-link>
+              <router-link
+                :to="{ name: 'volunteer-register' }"
+                class="font-medium link"
+                >Стати волонтером</router-link
+              >
             </p>
           </Form>
         </div>
--- src/components/authentication/Register.vue
+++ src/components/authentication/Register.vue
@@ -1,40 +1,46 @@
 <script setup>
-import { computed } from "vue"
-import { Form, Field, ErrorMessage } from 'vee-validate';
-import { useRouter, useRoute } from 'vue-router'
-import * as yup from 'yup';
-import { register } from '@plugins/firebase/auth.js'
-import { setData } from '@plugins/firebase/firestore.js'
-import { useAlertStore } from '@stores/alert.js'
-import { useUserStore } from '@stores/user.js'
+import { computed } from "vue";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { useRouter, useRoute } from "vue-router";
+import * as yup from "yup";
+import { register } from "@plugins/firebase/auth.js";
+import { setData } from "@plugins/firebase/firestore.js";
+import { useAlertStore } from "@stores/alert.js";
+import { useUserStore } from "@stores/user.js";
 
-const { authorized } = useUserStore()
-const alertStore = useAlertStore()
-const userStore = useUserStore()
-const router = useRouter()
-const route = useRoute()
+const { authorized } = useUserStore();
+const alertStore = useAlertStore();
+const userStore = useUserStore();
+const router = useRouter();
+const route = useRoute();
 
 const typeUser = computed(() => {
-  if (route.name === 'volunteer-register') {
-    return 'volunteer'
-  } else if (route.name === 'user-register') {
-    return 'user'
+  if (route.name === "volunteer-register") {
+    return "volunteer";
+  } else if (route.name === "user-register") {
+    return "user";
   }
-})
+});
 
 if (authorized) {
-  router.push("/profile")
+  router.push("/profile");
 }
 
 const schema = yup.object({
   name: yup.string().required("Заповніть поле"),
   email: yup.string().email("В правильний email").required("Заповніть поле"),
-  phone: yup.string().matches(/^[+]?[0-9]{10,15}$/, "Введіть правильний номер телефона").required("Заповніть поле"),
-  password: yup.string()
+  phone: yup
+    .string()
+    .matches(/^[+]?[0-9]{10,15}$/, "Введіть правильний номер телефона")
+    .required("Заповніть поле"),
+  password: yup
+    .string()
     .min(6, "Пароль має бути не менше 6 символів")
-    .trim().required("Заповніть поле"),
-  confirmPassword: yup.string()
-    .oneOf([yup.ref('password')], "Паролі мають співпадати") // Проверка на совпадение с password
+    .trim()
+    .required("Заповніть поле"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Паролі мають співпадати") // Проверка на совпадение с password
     .min(6, "Пароль має бути не менше 6 символів")
     .trim()
     .required("Заповніть поле"),
@@ -45,38 +51,38 @@ const form = [
     title: "Ваше ім'я",
     name: "name",
     placeholder: "Alex Muller",
-    type: "text"
+    type: "text",
   },
   {
     title: "Ваший email",
     name: "email",
     placeholder: "name@company.com",
-    type: "email"
+    type: "email",
   },
   {
     title: "Ваший номер телефона",
     name: "phone",
     placeholder: "+380986252683",
-    type: "text"
+    type: "text",
   },
   {
     title: "Пароль",
     name: "password",
     placeholder: "••••••••",
-    type: "password"
+    type: "password",
   },
   {
     title: "Підтвердіть пароль",
     name: "confirmPassword",
     placeholder: "••••••••",
-    type: "password"
-  }
-]
+    type: "password",
+  },
+];
 
 async function submit(values, { resetForm }) {
   try {
-    const user = await register(values.email, values.password)
-    const { accessToken, uid, email } = user
+    const user = await register(values.email, values.password);
+    const { accessToken, uid, email } = user;
 
     const userInfo = await setData(["users", uid], {
       uid,
@@ -85,14 +91,14 @@ async function submit(values, { resetForm }) {
       phone: values.phone,
       role: typeUser.value,
       news: false,
-      notification: false
-    })
+      notification: false,
+    });
 
-    alertStore.showAlert("Ви успішно зареєструвалися як волонтер!", "success")
-    resetForm()
-    router.push('/profile')
+    alertStore.showAlert("Ви успішно зареєструвалися як волонтер!", "success");
+    resetForm();
+    router.push("/profile");
   } catch (err) {
-    alertStore.showAlert(err.message, "error")
+    alertStore.showAlert(err.message, "error");
   }
 }
 </script>
@@ -101,29 +107,49 @@ async function submit(values, { resetForm }) {
 <template>
   <div class="register h-full flex items-center">
     <div class="container flex justify-center">
-      <div class="w-full bg-turquoise-900 rounded-lg shadow border-turquoise-700 sm:max-w-md xl:p-0">
+      <div
+        class="w-full my-[30px] bg-turquoise-900 rounded-lg shadow border-turquoise-700 sm:max-w-md xl:p-0"
+      >
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
-            {{ typeUser === 'volunteer' ? 'Станьне волонтером' : '' }}
-            {{ typeUser === 'user' ? 'Створіть акаунт' : '' }}
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl"
+          >
+            {{ typeUser === "volunteer" ? "Станьне волонтером" : "" }}
+            {{ typeUser === "user" ? "Створіть акаунт" : "" }}
           </h1>
-          <Form class="space-y-2 md:space-y-4" @submit="submit" :validation-schema="schema"
-            v-slot="{ errors, isSubmitting }">
+          <Form
+            class="space-y-2 md:space-y-4"
+            @submit="submit"
+            :validation-schema="schema"
+            v-slot="{ errors, isSubmitting }"
+          >
             <div v-for="fieldData in form" :key="fieldData.name">
-              <label :for="fieldData.name" class="block mb-2 text-sm font-medium text-white">
+              <label
+                :for="fieldData.name"
+                class="block mb-2 text-sm font-medium text-white"
+              >
                 {{ fieldData.title }}
               </label>
-              <Field :type="fieldData.type" :name="fieldData.name" :id="fieldData.name"
+              <Field
+                autocomplete
+                :type="fieldData.type"
+                :name="fieldData.name"
+                :id="fieldData.name"
                 class="bg-turquoise-700 border-turquoise-600 text-white text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-full p-2.5 placeholder-turquoise-400"
-                :class="errors[fieldData.name] && 'border-red-400'" :placeholder="fieldData.placeholder" required />
+                :class="errors[fieldData.name] && 'border-red-400'"
+                :placeholder="fieldData.placeholder"
+                required
+              />
               <ErrorMessage :name="fieldData.name" class="text-red-400" />
             </div>
             <button :disabled="isSubmitting" type="submit" class="btn w-full">
               Cтворити акаунт
             </button>
             <p class="text-sm font-light text-turquoise-400">
-              Вже маєте акаунт? <router-link to="/join/login" class="font-medium link">Увійдіть
-                тут</router-link>
+              Вже маєте акаунт?
+              <router-link to="/join/login" class="font-medium link"
+                >Увійдіть тут</router-link
+              >
             </p>
           </Form>
         </div>
